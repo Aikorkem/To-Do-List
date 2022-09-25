c14d87bdc43bcb62a14e2e7f25ac0acb371fe644
Aikorkem <zhumabekaks@gmail.com>
2022-09-25 21:01:42 +0600

    Created new Lists and added new data collections

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 const Item = mongoose.model("Item", {name: {type: String, required: true}});
+const List = mongoose.model("List", {name: String, items: [{name: String}]});
 
 const item1 = new Item({
     name: "First item"
@@ -28,7 +30,7 @@ const item2 = new Item({
     name: "Second item"
 });
 
-
+const defaultItems = [item1, item2];
 
 app.get("/",function(req, res){
     let day = date.getDate();  
@@ -41,31 +43,73 @@ app.get("/",function(req, res){
 
             if(results.length === 0){
                 //inserting default entries if collection is empty
-                Item.insertMany([item1,item2], function(error){
+                Item.insertMany(defaultItems, function(error){
                     if( error ){
                         console.log(error);
                     }else{
-                        console.log("Successs");
+                        console.log("Success");
                     }
                 });
             }
 
             // templating with EJS, list.ejs file in views directory
-            res.render('list', {listTitle: day, items: results});
+            res.render('list', {listTitle: "Today", items: results});
         }
     });    
+});
+
+app.get("/:title", function(req,res){
+
+    const title = _.capitalize(req.params.title);
+
+    List.findOne({name: title}, function(error, results){
+        if(!error){
+            if(!results){
+                const list = new List({
+                    name: title,
+                    items: defaultItems
+                });
+            
+                list.save();
+                res.redirect("/" + title);
+            }else{
+                res.render("list", {listTitle: results.name, items: results.items});
+            }
+
+            
+        }else{
+            console.log(error);
+        }
+
+    });
+    
+    
 });
 
 //Getting the entered data
 app.post("/", function(request, response){
     const itemName = request.body.newItem;
-
+    const listTitle = request.body.button;
+    let day = date.getDate(); 
     const item = new Item({
         name: itemName
     });
 
-    item.save();
-    response.redirect("/");
+    if(listTitle === "Today"){
+        item.save();
+        response.redirect("/");
+    }else{
+        List.findOne({name: listTitle}, function(error,results){
+            if(error){
+                console.log(error);
+            }else{
+                results.items.push(item);
+                results.save();
+                response.redirect("/" + listTitle);
+            }
+        });
+    }
+    
 
     //Acquiring data without database
     // let title = request.body.button;
@@ -82,19 +126,34 @@ app.post("/", function(request, response){
 
 app.post("/delete", function(req,res){
     const checkedID = req.body.checkbox;
+    const listTitle = req.body.listName;
 
-    Item.findByIdAndRemove(checkedID, function(error){
-        if(error){
-            console.log(error)
-        }else{
-            console.log("Deleted id - " + checkedID);
-        }
-    });
+    if(listTitle === "Today"){
+        Item.findByIdAndRemove(checkedID, function(error){
+            if(error){
+                console.log(error)
+            }else{
+                console.log("Deleted id - " + checkedID);
+            }
+        });
+        
+        res.redirect("/");
+    }else{
+        List.findOneAndUpdate(
+            {name: listTitle},
+            {$pull: {items: {_id: checkedID}}},
+            function(error){
+                if(error) {
+                    console.log(error);
+                }else{
+                    console.log("Deleted id - " + checkedID);
+                }
+            }
+        );
+
+        res.redirect("/" + listTitle);
+    }
     
-    res.redirect("/");
-});
-app.get("/work", function(req,res){
-    res.render("list", {listTitle: "Work List", items: workItems});
 });
 
 app.get("/about", function(req,res){
